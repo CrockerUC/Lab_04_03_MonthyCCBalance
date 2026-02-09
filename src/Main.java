cf67d38827be9b5bb32467bfaafadfbb6b51bf25
CrockerUC <crockecf@mail.uc.edu>
2026-02-09 14:54:41 -0500

    Declared/initialized variables, calculated interest due, and output results. Finished Lab 4 Task 3.

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,12 @@ public class Main
 {
     public static void main(String[] args)
     {
-        
+        double creditCardBalance = 5000;
+        double interestRate = .17;
+
+        System.out.println("Your credit card balance is $" + creditCardBalance + " and the interest rate is " + (int) (interestRate * 100) + "%.");
+        System.out.println("Interest due after one month is $" + creditCardBalance * interestRate + ".");
+        creditCardBalance += creditCardBalance * interestRate;
+        System.out.println("Interest due after two months is $" + creditCardBalance * interestRate + ".");
     }
 }
